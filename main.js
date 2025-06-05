f9992a24d67dc59cb07b8509e97d59bc228c73a1
Jitta Yashwanth Reddy <147270835+Yashwanth9898GitHub@users.noreply.github.com>
2025-06-05 17:47:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var TablePrint = /** @class */ (function () {
+    function TablePrint() {
+    }
+    TablePrint.prototype.printTable = function (tableNumber) {
+        var result = '';
+        for (var count = 1; count <= 10; count++) {
+            result += "".concat(tableNumber, " * ").concat(count, " = ").concat(tableNumber * count, "<br>");
+        }
+        return result;
+    };
+    return TablePrint;
+}());
+function generateTable() {
+    var input = document.getElementById('inputNumber');
+    var output = document.getElementById('output');
+    var value = parseInt(input.value);
+    if (!isNaN(value) && input.value.trim().length <= 16) {
+        var table = new TablePrint();
+        output.innerHTML = table.printTable(value);
+    }
+    else {
+        output.innerHTML = "Please enter a valid number.";
+    }
+}
+// Make it globally accessible for button onclick
+window.generateTable = generateTable;
